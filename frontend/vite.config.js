3e43b50c8aa5c636d00470e9b3737280f742209d
Harshi-coder17 <harshisingla17@gmail.com>
2026-03-26 13:44:17 +0530

    Updates done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-<<<<<<< HEAD
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -14,9 +13,8 @@ export default defineConfig({
     },
   },
 })
-=======
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 73ffa1e (frontend updated)
+
+
+
